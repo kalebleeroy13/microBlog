a98a72105ad34e8cf95cd69d2fe38b602095c360
khuunter@yahoo.com <khuunter@yahoo.com>
2023-10-02 23:16:11 +0700

    typo fix, add user login form/and template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -16,4 +17,10 @@ def index():
             'body': 'The avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts) 
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login') 
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
