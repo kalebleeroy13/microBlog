59cd44a445a43231d753a9fade16f79dc5b09c48
khuunter@yahoo.com <khuunter@yahoo.com>
2023-10-16 00:13:22 +0700

    fin login logic need to fix usr profile error

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from flask_login import UserMixin
 from app import db, login 
 
 
-
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
