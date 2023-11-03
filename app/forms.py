a9a05c4d43f8d1f5e1dd3f92160fd13b5822987c
khuunter@yahoo.com <khuunter@yahoo.com>
2023-11-03 22:34:18 +0700

    ch.9 pagination sub & display blogpost

--- app/forms.py
+++ app/forms.py
@@ -35,7 +35,7 @@ class RegistrationForm(FlaskForm):
 class EditProfileForm(FlaskForm):
 	username = StringField('Username', validators= [DataRequired()])
 	about_me = TextAreaField('About me', validators =[Length(min=0, max=140)])
-	submit =SubmitField('Submit')
+	submit = SubmitField('Submit')
 
 	def __init__(self, original_username, *args, **kwargs):
 		super(EditProfileForm,self).__init__(*args, **kwargs)
@@ -45,4 +45,10 @@ class EditProfileForm(FlaskForm):
 		if username.data != self.original_username:
 			user = User.query.filter_by(username=username.data).first()
 			if user is not None:
-				raise ValidationError('Please use a different email address.')
+				raise ValidationError('Please use a different username.')
+
+
+class PostForm(FlaskForm):
+	post = TextAreaField('Say something', validators=[
+		DataRequired(), Length(min=1, max=140)])
+	submit = SubmitField('Submit')
