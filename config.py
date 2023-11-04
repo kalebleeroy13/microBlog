27e9473c693f629e056d36a84dbd3d541fd48f1e
khuunter@yahoo.com <khuunter@yahoo.com>
2023-11-04 14:42:33 +0700

    ability to find other memebers

--- config.py
+++ config.py
@@ -11,4 +11,5 @@ class Config(object):
 	MAIL_USE_TLS =	os.environ.get('MAIL_USE_TLS') is not None
 	MAIL_USERNAME =	os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['khuunter@yahoo.com'] 
+	ADMINS = ['khuunter@yahoo.com']
+	POSTS_PER_PAGE = 3 
