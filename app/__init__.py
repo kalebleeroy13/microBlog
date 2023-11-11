98d44311c5be0b1efecdb9320c21f766129b07e0
khuunter@yahoo.com <khuunter@yahoo.com>
2023-11-11 18:22:35 +0700

    ch.11 bootstrap facelift pt.2

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+
 from config import Config
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap =Bootstrap(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
--- app/models.py
+++ app/models.py
@@ -14,7 +14,6 @@ followers = db.Table(
 )
 
 
-
 class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), index=True, unique=True)
