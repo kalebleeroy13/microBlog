e000ee7e4032b3b24e22f4118e166d15273c43d3
khuunter@yahoo.com <khuunter@yahoo.com>
2023-11-12 00:04:56 +0700

    ch.12 dates and timestamps added

--- app/routes.py
+++ app/routes.py
@@ -135,8 +135,8 @@ def user(username):
 	prev_url = url_for('index', page=posts.prev_num) \
 		if posts.has_prev else None
 	form = EmptyForm()
-	return render_template('index.html', title='Explore', posts=posts.items, 
-			next_url=next_url, prev_url=prev_url)
+	return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
