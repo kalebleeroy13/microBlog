b7842c11368a4cb0c91df2a56c93550a6298b408
khuunter@yahoo.com <khuunter@yahoo.com>
2023-11-29 18:10:39 +0700

    added cli.py

--- app/routes.py
+++ app/routes.py
@@ -15,6 +15,7 @@ def before_request():
 	if current_user.is_authenticated:
 		current_user.last_seen = datetime.utcnow()
 		db.session.commit()
+	g.locale = str(get_locale())
 
 
 @app.route('/', methods=['GET', 'POST'])
