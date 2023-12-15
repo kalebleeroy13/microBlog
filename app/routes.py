7c9baacc9f2564239e78e3047a565037adfa3f40
khuunter@yahoo.com <khuunter@yahoo.com>
2023-12-15 18:28:31 +0700

    babel.cfg file fix, translate file added + ajax logic, translate link

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,7 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
     EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
+from app.translate import translate
 
 
 @app.before_request
@@ -212,3 +213,12 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    data = request.get_json()
+    return {'text': translate(data['text'],
+                              data['source_language'],
+                              data['dest_language'])}
